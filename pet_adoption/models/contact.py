4933263f4e94e0ca10a4039bf9fafd4e4c6208f7
Forhath-Hosain-Ome <omehasan3@gmail.com>
2025-12-31 20:08:28 +0600

    contact model updated v.01

--- pet_adoption/models/contact.py
+++ pet_adoption/models/contact.py
@@ -3,9 +3,18 @@ from django.db import models
 class Contact(models.Model):
     """Model for storing contact form submissions"""
     
+    SUBJECT_CHOICES = [
+        ('adoption', 'Adoption Inquiry'),
+        ('volunteering', 'Volunteering'),
+        ('donation', 'Donation Support'),
+        ('feedback', 'Feedback'),
+        ('other', 'Other'),
+    ]
+    
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20, blank=True, null=True)
     email = models.EmailField()
+    subject = models.CharField(max_length=50, choices=SUBJECT_CHOICES, default='other')
     message = models.TextField()
     
     # Status tracking
